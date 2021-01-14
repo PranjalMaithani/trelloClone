da84e7a09d6d04e23563bd1017d802608626bf8d
Pranjal Maithani <maithanipranjal@gmail.com>
2021-01-14 17:59:12 +0530

    Header title bug fixed
    Fixed bug where the board title could show up on login screen

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <header className="App-header">
       <div className="headerTab">
-        {currentBoard !== null && (
+        {currentBoard !== null && match.params.b && (
           <div className="boardUIbuttons">
             <Link
               to="/boards"
